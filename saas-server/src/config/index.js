a51763152c6a276738453331b9077dcb1be263fa
Chinmaya-AAS <it_chinmaya@outlook.com>
2025-03-24 18:06:21 +0530

    end of server  in 23/march

--- saas-server/src/config/index.js
+++ saas-server/src/config/index.js
@@ -35,7 +35,20 @@ const config = {
   },
   corsOrigins: process.env.CORS_ORIGINS ? 
     process.env.CORS_ORIGINS.split(',') : 
-    ['http://localhost:3000']
+    [
+      'http://localhost:3000',
+      'http://localhost:3001',
+      'http://localhost:3002',
+      'http://localhost:3003',
+      'http://localhost:3004',
+      'http://localhost:3005',
+      'http://localhost:3006',
+      'http://localhost:3007',
+      'http://localhost:3008',
+      'http://localhost:3009',
+      'http://localhost:3010'
+    ]
+    
 };
 
 module.exports = config;
